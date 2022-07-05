5f118d285876b00c9db8fa6ff78b268fc75d5b1b
Hassyunity <hassy.tsihoarana@gmail.com>
2022-07-05 05:50:14 +0300

    setup app

---
+++ src/Navbar.js
@@ -0,0 +1,43 @@
+import React, {useState, useEffect} from 'react'
+import "./Navbar.css"
+export default function navbar() {
+
+//   const [toggleMenu, setToogleMenu] = useState(false)
+//   const [largeur, setLargeur] = useState(window.innerWidth);
+
+//   const toogleNavSmallScreen = () => {
+//     setToogleMenu(!toggleMenu);
+//   }
+
+//   useEffect(() => {
+
+//     const changeWidth = () => {
+//         setlargeur(window.innerWidth);
+
+//         // if(window.innerWidth > 500){
+//         //     setToogleMenu(false);
+//         // }
+//     }
+
+//     window.addEventListener('resize', changeWidth);
+
+//     return () => {
+//         window.removeEventListener('resize', changeWidth);
+//     }
+//   }, [])
+  return (
+    <nav>
+        {/* {toggleMenu && (
+            
+        )} */}
+        <ul className='liste'>
+                <li className='items'>Home</li>
+                <li className='items'>Todo</li>
+                <li className='items'>Posts</li>
+                <li className='items'>Users</li>
+            </ul>
+        
+        <button className='btn'>BTN</button>
+    </nav>
+  )
+}
---
+++ src/components/menuItems.js
@@ -0,0 +1,27 @@
+export const MenuItems = [
+    {
+      Title: "Home",
+      url: "#",
+      cName: "nav-links"
+    },
+    {
+      Title: "todo",
+      url: "#",
+      cName: "nav-links"
+    },
+    {
+      Title: "post",
+      url: "#",
+      cName: "nav-links"
+    },
+    {
+      Title: "users",
+      url: "#",
+      cName: "nav-links"
+    },
+    {
+      Title: "Sign Up",
+      url: "#",
+      cName: "nav-links-mobile"
+    }
+  ];
