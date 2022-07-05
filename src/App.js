8e2d0e52ffcc9d20397005f89afdc287c15fe7a6
HassyUnity <hassy.tsihoarana@gmail.com>
2022-07-05 16:19:21 +0300

    create database setup

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 
 import DatatablePage from './components/Table';
 import TodoList from './components/TodoList';
+import renderForm from './components/Login';
 // import MyForm from './components/Post';
 
 
@@ -15,11 +16,32 @@ function App() {
       <Navbar />
       <TodoList />
       <DatatablePage />
+      <renderForm />
       {/* <MyForm /> */}
+      <div className="form">
+        <form>
+          <div className="input-container">
+            <label>Username </label>
+            <input type="text" name="uname" required />
+            {/* {renderErrorMessage("uname")} */}
+          </div>
+          <div className="input-container">
+            <label>Password </label>
+            <input type="password" name="pass" required />
+            {/* {renderErrorMessage("pass")} */}
+          </div>
+          <div className="button-container">
+            <input type="submit" />
+          </div>
+        </form>
+      </div>
     </div>
 
   );
 }
+
+
+
 
 export default App;
 
