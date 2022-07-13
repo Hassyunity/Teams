ef6d436c6e7814cce71967f032e1788d8689ed4e
HassyUnity <hassy.tsihoarana@gmail.com>
2022-07-13 15:40:49 +0300

    create navbar menu

--- src/App.js
+++ src/App.js
@@ -1,47 +1,79 @@
 import React from 'react';
-import Navbar from './Navbar';
 import './App.css';
-
-import DatatablePage from './components/Table';
-import TodoList from './components/TodoList';
-import renderForm from './components/Login';
-// import MyForm from './components/Post';
-
-
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './pages';
+import About from './pages/about';
+import Events from './pages/events';
+import AnnualReport from './pages/annual';
+import Teams from './pages/team';
+import Blogs from './pages/blogs';
+import SignUp from './pages/signup';
 
 function App() {
   return (
-
-    <div className="App">
+    <Router>
       <Navbar />
-      <TodoList />
-      <DatatablePage />
-      <renderForm />
-      {/* <MyForm /> */}
-      <div className="form">
-        <form>
-          <div className="input-container">
-            <label>Username </label>
-            <input type="text" name="uname" required />
-            {/* {renderErrorMessage("uname")} */}
-          </div>
-          <div className="input-container">
-            <label>Password </label>
-            <input type="password" name="pass" required />
-            {/* {renderErrorMessage("pass")} */}
-          </div>
-          <div className="button-container">
-            <input type="submit" />
-          </div>
-        </form>
-      </div>
-    </div>
-
+      <Switch>
+        <Route path='/' exact component={Home} />
+        <Route path='/about' component={About} />
+        <Route path='/events' component={Events} />
+        <Route path='/annual' component={AnnualReport} />
+        <Route path='/team' component={Teams} />
+        <Route path='/blogs' component={Blogs} />
+        <Route path='/sign-up' component={SignUp} />
+      </Switch>
+    </Router>
   );
 }
 
-
-
-
 export default App;
+
+
+// import React from 'react';
+// import Navbar from './Navbar';
+// import './App.css';
+
+// import DatatablePage from './components/Table';
+// import TodoList from './components/TodoList';
+// import renderForm from './components/Login';
+// // import MyForm from './components/Post';
+
+
+
+// function App() {
+//   return (
+
+//     <div className="App">
+//       <Navbar />
+//       <TodoList />
+//       <DatatablePage />
+//       <renderForm />
+//       {/* <MyForm /> */}
+//       <div className="form">
+//         <form>
+//           <div className="input-container">
+//             <label>Username </label>
+//             <input type="text" name="uname" required />
+//             {/* {renderErrorMessage("uname")} */}
+//           </div>
+//           <div className="input-container">
+//             <label>Password </label>
+//             <input type="password" name="pass" required />
+//             {/* {renderErrorMessage("pass")} */}
+//           </div>
+//           <div className="button-container">
+//             <input type="submit" />
+//           </div>
+//         </form>
+//       </div>
+//     </div>
+
+//   );
+// }
+
+
+
+
+// export default App;
 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
 import React from 'react';
-import '@fortawesome/fontawesome-free/css/all.min.css';
-import 'bootstrap-css-only/css/bootstrap.min.css';
-import 'mdbreact/dist/css/mdb.css';
 import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+<React.StrictMode>
+	<App />
+</React.StrictMode>,
+document.getElementById('root')
 );
+
+
+
+// import React from 'react';
+// import '@fortawesome/fontawesome-free/css/all.min.css';
+// import 'bootstrap-css-only/css/bootstrap.min.css';
+// import 'mdbreact/dist/css/mdb.css';
+// import ReactDOM from 'react-dom';
+// import App from './App';
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
