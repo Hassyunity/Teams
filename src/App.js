2baf25ea71714a09e723df170acec62baad2fb74
HassyUnity <hassy.tsihoarana@gmail.com>
2022-07-14 18:46:08 +0300

    login improvement

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import './App.css';
-import './home.js'
-
 // import useStateArray from 'react';
 
 import BasicForm from './components/forms/basicForm';
-import Appp from './home.js';
 
 const App = () => {
   return (
