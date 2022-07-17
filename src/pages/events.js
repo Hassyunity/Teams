46c6d4920407b090e0b16afd0ef441fe33df0c48
Hassyunity <hassy.tsihoarana@gmail.com>
2022-07-17 03:37:34 +0300

    test chatbox message

--- src/pages/events.js
+++ src/pages/events.js
@@ -1,4 +1,14 @@
 import React from 'react';
+import "react-chat-elements/dist/main.css"
+import { MessageBox } from "react-chat-elements";
+import { ChatList } from "react-chat-elements";
+import { MessageList } from "react-chat-elements";
+import { ChatItem } from "react-chat-elements";
+import { Dropdown } from "react-chat-elements";
+import { IconContext } from 'react-icons';
+import { Input } from 'react-chat-elements'
+
+
 
 const Events = () => {
   return (
@@ -11,7 +21,33 @@ const Events = () => {
       }}
     >
       <h1>Welcome to GeeksforGeeks Events</h1>
+       <table>
+        <tr>
+        <td>
+          
+        </td>
+        </tr>
+      <tr>
+        <td>
+            <ChatItem
+              avatar="https://avatars.githubusercontent.com/u/41473129?v=4"
+              alt="kursat_avatar"
+              title="Emre"
+              subtitle="What are you doing ?"
+              date={new Date()}
+              muted={true}
+              showMute={true}
+              showVideoCall={true}
+              unread={2}
+            />
+          </td>
+      </tr>
+    </table>
+      
+      
     </div>
+    
+    
   );
 };
 
